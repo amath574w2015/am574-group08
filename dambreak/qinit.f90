17f043dc3ffd07a166b00c625f46638f00a0ee0e
xinshengqin <xinshengqin2014@gmail.com>
2015-02-23 23:49:30 -0800

    some further modification for I.C. But the code is still not working.

--- dambreak/qinit.f90
+++ dambreak/qinit.f90
@@ -20,16 +20,31 @@ subroutine qinit(meqn,mbc,mx,my,xlower,ylower,dx,dy,q,maux,aux)
     ! Set flat state based on sea_level
     q = 0.d0
     !discharge = 0.115d0 * 0.054  ! free stream
-    forall(i=1:mx, j=1:my)
+    do i=1,mx
         xcenter = xlower + (i - 0.5d0) * dx
-        if (xcenter < sloc) then
-            q(1,i,j) = hl
-            q(2,i,j) = hl*ul
-        else
-            q(1,i,j) = hr
-            q(2,i,j) = hr*ur
-        !q(1,i,j) = max(0.d0, sea_level - aux(1,i,j))
-        !q(2,i,j) = discharge   ! 0.115d0 * q(1,i,j)
-    end forall
+        do j=1,my
+            if (xcenter < sloc) then
+                q(1,i,j) = hl
+                q(2,i,j) = hl*ul
+            else
+                q(1,i,j) = hr
+                q(2,i,j) = hr*ur
+            endif
+        enddo
+    enddo
+
+!The grammar below seem to be incorrect for *.f90. It's only correct fo *.f
+!    forall(i=1:mx, j=1:my)
+!        xcenter = xlower + (i - 0.5d0) * dx
+!        if (xcenter < sloc) then
+!            q(1,i,j) = hl
+!            q(2,i,j) = hl*ul
+!        else
+!            q(1,i,j) = hr
+!            q(2,i,j) = hr*ur
+!        !q(1,i,j) = max(0.d0, sea_level - aux(1,i,j))
+!        !q(2,i,j) = discharge   ! 0.115d0 * q(1,i,j)
+!        endif
+!    end forall
 
 end subroutine qinit
--- dambreak/setrun.py
+++ dambreak/setrun.py
@@ -131,8 +131,8 @@ def setrun(claw_pkg='geoclaw'):
 
     if clawdata.output_style==1:
         # Output nout frames at equally spaced times up to tfinal:
-        clawdata.num_output_times = 50
-        clawdata.tfinal = 200.
+        clawdata.num_output_times = 5
+        clawdata.tfinal = 10.
         clawdata.output_t0 = True  # output at initial (or restart) time?
 
     elif clawdata.output_style == 2:
